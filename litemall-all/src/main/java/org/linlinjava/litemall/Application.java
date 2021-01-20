2070dfde2446640e480c79f3ba217bce227cd487
will <duanshuyong0@163.com>
2021-01-20 16:32:06 +0800

    m

--- litemall-all/src/main/java/org/linlinjava/litemall/Application.java
+++ litemall-all/src/main/java/org/linlinjava/litemall/Application.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 public class Application {
 
-    public static void main(String[] args) throws Exception {
+     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
